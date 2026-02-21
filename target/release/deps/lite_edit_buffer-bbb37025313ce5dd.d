23301d8472300c2bdba06cea6f190f457fd951aa
Brian Taylor <el.wubo@gmail.com>
2026-02-21 12:51:29 -0500

    feat: chunk text_buffer

---
+++ target/release/deps/lite_edit_buffer-bbb37025313ce5dd.d
@@ -0,0 +1,11 @@
+/Users/btaylor/Projects/lite-edit/.ve/chunks/text_buffer/worktree/target/release/deps/lite_edit_buffer-bbb37025313ce5dd.d: src/lib.rs src/gap_buffer.rs src/line_index.rs src/text_buffer.rs src/types.rs
+
+/Users/btaylor/Projects/lite-edit/.ve/chunks/text_buffer/worktree/target/release/deps/liblite_edit_buffer-bbb37025313ce5dd.rlib: src/lib.rs src/gap_buffer.rs src/line_index.rs src/text_buffer.rs src/types.rs
+
+/Users/btaylor/Projects/lite-edit/.ve/chunks/text_buffer/worktree/target/release/deps/liblite_edit_buffer-bbb37025313ce5dd.rmeta: src/lib.rs src/gap_buffer.rs src/line_index.rs src/text_buffer.rs src/types.rs
+
+src/lib.rs:
+src/gap_buffer.rs:
+src/line_index.rs:
+src/text_buffer.rs:
+src/types.rs:
